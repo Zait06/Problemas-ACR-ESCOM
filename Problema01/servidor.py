2f189b3859f0b3e6906c650bed4b92c0bed9faf6
Zait06 <zait-hdez@hotmail.com>
2019-09-15 17:09:26 -0500

    Servidor TCP

--- Problema01/servidor.py
+++ Problema01/servidor.py
@@ -1,10 +1,15 @@
 import os
 import sys
 import select
+import threading
+from playsound import playsound		# Librería para reproducir cancion
 
 class Servidor():
 	def __init__(self):
-		self.HOST=""; self.PORT=8080
+		self.HOST="localhost"; self.PORT=8080
+		self.NOMBRE_ARCHIVO = "Amiga.mp3"
+		print("Reproducciendo audio...")
+		playsound(self.NOMBRE_ARCHIVO)
 		
 
 

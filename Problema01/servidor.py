70ab8d72a470fc78d88980bba1abffd29eb674a7
Zait06 <zait-hdez@hotmail.com>
2019-09-15 17:15:45 -0500

    Servidor TCP

--- Problema01/servidor.py
+++ Problema01/servidor.py
@@ -8,9 +8,17 @@ class Servidor():
 	def __init__(self):
 		self.HOST="localhost"; self.PORT=8080
 		self.NOMBRE_ARCHIVO = "Amiga.mp3"
+		self.serveraddr=(self.HOST,self.PORT)
+
+		# Reproduccion de una cancion
 		print("Reproducciendo audio...")
 		playsound(self.NOMBRE_ARCHIVO)
+		print("Fin del audio")
 		
-
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.ServerTCP:
+    		self.ServerTCP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    		self.ServerTCP.bind(self.serveraddr)
+    		self.ServerTCP.listen(10)
+    		print("Servidor TCP a la escucha con direccion IP "+str(HOST))
 
 s=Servidor()

03593c3da8feb576d9cabf014036236da238c951
Zait06 <zait-hdez@hotmail.com>
2019-09-15 20:18:16 -0500

    Servidor TCP

--- Problema01/servidor.py
+++ Problema01/servidor.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import socket
 import select
 import threading
 from playsound import playsound		# Librería para reproducir cancion
@@ -16,43 +17,43 @@ class Servidor():
 			self.ServerTCP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
 			self.ServerTCP.bind(self.serveraddr)
 			self.ServerTCP.listen(10)
-		        print("Servidor TCP a la escucha con direccion IP "+str(self.HOST))
+			print("Servidor TCP a la escucha con direccion IP "+str(self.HOST))
 	
 			# Reproduccion de un audio
 			print("\nReproducciendo audio...")
-	    		playsound(self.NOMBRE_ARCHIVO)
-	    		print("Fin del audio")
+			playsound(self.NOMBRE_ARCHIVO)
+			print("Fin del audio")
 	
-	    		# self.servirPorSiempre()
+	    	# self.servirPorSiempre()
 
 	def servirPorSiempre(self):
-	    	try:
-    			while True:
-	    			conn,addr=self.ServerTCP.accept()	# Conección y direccion del ciente
-	    			print("Conectado a: ", addr)
-	    			self.listConec.append(conn)
-	    			hilo_leer=threading.Tread(target=self.recibir_instruccion,args=[conn,addr])
-	    			hilo_leer.start()
-	    			self.gestion_conexiones()
-	    	except Exception as e:
-	    		print(e)
+	    try:
+    		while True:
+	    		conn,addr=self.ServerTCP.accept()	# Conección y direccion del ciente
+	    		print("Conectado a: ", addr)
+	    		self.listConec.append(conn)
+	    		hilo_leer=threading.Tread(target=self.recibir_instruccion,args=[conn,addr])
+	    		hilo_leer.start()
+	    		self.gestion_conexiones()
+	    except Exception as e:
+	    	print(e)
 	
 	def gestion_conexiones(self):
-	    	for conn in self.listConec:
-	    		if conn.fileno()==-1:
-	    			self.listConec.remove(conn)
+	    for conn in self.listConec:
+	    	if conn.fileno()==-1:
+	    		self.listConec.remove(conn)
 
 	def recibir_instruccion(self,conn,addr):
-	    	try:
-	    		cur_thread=threading.cur_thread()
-	    		print("Recibiendo datos del cliente {} en el {}".format(addr, cur_thread.name))
-	    		while True:
-	    			data=conn.recv(self.buffer)
-	    			if data:
-	    				# Ejecutamos lectura
-	    	except Exception as e:
-	    		print(e)
-	    	finally:
-	    		conn.close()
+		try:
+			cur_thread=threading.current_thread()
+			print("Recibiendo datos del cliente {} en el {}".format(addr,cur_thread))
+			while True:
+				data=conn.recv(self.buffer)
+				if data:
+					print("Instruccion recibido")
+		except Exception as e:
+			print(e)
+		finally:
+			conn.close()
 
 s=Servidor()

ff756f7fc25bf9fc7eac73f8fa111bb6b9cee08c
Zait06 <zait-hdez@hotmail.com>
2019-09-28 12:37:16 -0600

    Servidor

--- Problema01/servidor.py
+++ Problema01/servidor.py
@@ -2,13 +2,18 @@ import os
 import sys
 import socket
 import select
+import logging
 import threading
-from playsound import playsound		# Librer�a para reproducir cancion
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='(%(threadName)-10s) %(message)s',
+)
 
 class Servidor():
     def __init__(self):
-        self.HOST="localhost"; self.PORT=8080
-        self.buffer=1024	# A cambiar
+        self.HOST="127.0.0.1"; self.PORT=8080
+        self.buffer=1024; self.juga=0
         self.NOMBRE_ARCHIVO="Amiga.mp3"
         self.serveraddr=(self.HOST,self.PORT)
         self.listConec=list()	# Lista de conexiones recibidas
@@ -17,30 +22,26 @@ class Servidor():
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.ServerTCP:
             self.ServerTCP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
             self.ServerTCP.bind(self.serveraddr)
-            self.ServerTCP.listen(10)
+            self.ServerTCP.listen(5)
             print("Servidor TCP a la escucha con direccion IP "+str(self.HOST))
-    
-            # Reproduccion de un audio
-            print("\nReproducciendo audio...")
-            # playsound(self.NOMBRE_ARCHIVO)
-            print("Fin del audio")
-
             self.servirPorSiempre(self.ServerTCP)
 
     def servirPorSiempre(self,servidor):
-        print("Esperando instruccion...")
+        logging.debug("Esperando instruccion...")
         try:
             while True:
-                conn,addr=servidor.accept()	# Conecci�n y direccion del ciente
-                print("Conectado a: ", addr)
+                conn,addr=servidor.accept()	# Coneccion y direccion del ciente
+                logging.debug("Conectado a: ", addr)
                 self.listConec.append(conn)
-                hilo_leer=threading.Tread(target=self.recibir_instruccion,args=[conn,addr])
-                hilo_leer.start()
+                self.juga+=1
+                hilo_jugador=threading.Tread(target=self.recibir_instruccion,
+                                                args=[conn,addr],
+                                                name="Jugador-"+str(self.juga))
+                hilo_jugador.start()
                 self.gestion_conexiones()
         except Exception as e:
             print(e)
 
-    
     def gestion_conexiones(self):
         for conn in self.listConec:
             if conn.fileno()==-1:
@@ -49,7 +50,7 @@ class Servidor():
     def recibir_instruccion(self,conn,addr):
         try:
             cur_thread=threading.current_thread()
-            print("Recibiendo datos del cliente {} en el {}".format(addr,cur_thread))
+            logging.debug("Recibiendo datos del cliente {} en el {}".format(addr,cur_thread))
             while True:
                 data=conn.recv(self.buffer)
                 if data:

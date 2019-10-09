d1ab5223da53d6ced9077456e6686ebfdc6cb138
Zait06 <zait-hdez@hotmail.com>
2019-10-09 14:10:51 -0500

    Cliente TCP

--- ProtocoloFTP/cliente/cliente.py
+++ ProtocoloFTP/cliente/cliente.py
@@ -9,20 +9,23 @@ class Cliente():
         self.PORT=2121
 
         ftp = FTP()
-        ftp.connect('127.0.0.1', 2121)
+        ftp.connect(self.HOST, self.PORT)
         ftp.login(usu, contra, "noaccount")
         try:
             with open('README_COPIA.txt', 'wb') as f:
                 ftp.retrbinary('RETR README.txt', f.write)
                 f.write(str.encode(time.strftime("%H:%M:%S")))
+            a=input("Nombre de la carpeta nueva: ")
+            ftp.mkd(a)
         except Exception as e:
             print(e)
         finally:
+            print("Tarea finalizada")
             ftp.quit()
 
 print("Hola\nQuiere ingresar usuario y contrasenia?\n[S]i\t[N]o")
 op=input("Opcion: ")
-if op=="S":
+if op=="S" or op=='s':
     name=input("Usuario: ")
     contra=input("Contrasenia: ")
     c=Cliente(name, contra)   

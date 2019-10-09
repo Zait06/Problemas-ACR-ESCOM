6d47cc0989732bf309d7e9c51f78c0a4a8c69d3b
Zait06 <zait-hdez@hotmail.com>
2019-10-09 07:38:37 -0500

    Servidor FTP

--- ProtocoloFTP/servidor/servidorFTP.py
+++ ProtocoloFTP/servidor/servidorFTP.py
@@ -9,30 +9,27 @@ def main():
     # Instancia un autorizador dummy para controlar usuarios "virtuales"
     authorizer = DummyAuthorizer()
 
-    # Define un nuevo usuario teniendo todos los permisos y otro para 
-    # usuarios de solo lectura
+    # Define un nuevo usuario teniendo todos los permisos y otro para usuarios de solo lectura
     authorizer.add_user('user', '12345', '.', perm='elradfmwMT')
-    authorizer.add_anonymous(os.getcwd())
+    authorizer.add_anonymous(os.getcwd())   # Obtener la direcccion del archivo actual
 
     # Instancia una clase controladora de FTP
     handler = FTPHandler
     handler.authorizer = authorizer
 
-    # Define una string predeterminada para cuando alguien se conecte
-    # al cliente
+    # Define un string predeterminado para cuando alguien se conecte al cliente
     handler.banner = 'pyftpdlib basado en FTP, listo'
 
     logging.basicConfig(filename='pyftpd.log', level=logging.INFO)
 
-    # Instancia una clase servidor FTP y abre conexión en 
-    # 0.0.0.0:2121
+    # Instancia una clase servidor FTP y abre conexión en 0.0.0.0:2121
     address = ('127.0.0.1', 2121)
     server = FTPServer(address, handler)
 
     # configura un limite de conexiones
     server.max_cons = 256
     server.max_cons_per_ip = 5
-    
+
     # Inicia el servidor FTP
     server.serve_forever()
 

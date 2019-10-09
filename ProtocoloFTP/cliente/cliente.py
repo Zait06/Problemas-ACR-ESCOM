541094846dacf7c2896ecfd59a5bf3b9cedcd60d
Zait06 <zait-hdez@hotmail.com>
2019-10-09 12:13:14 -0500

    Cliente FTP

--- ProtocoloFTP/cliente/cliente.py
+++ ProtocoloFTP/cliente/cliente.py
@@ -1,7 +1,30 @@
 from ftplib import FTP
-ftp = FTP('ftp.debian.org') 
-ftp.login()
-ftp.cwd('debian')
-ftp.retrlines('LIST')
-ftp.retrbinary('RETR README', open('README', 'wb').write)
-ftp.quit()
+import os
+import sys
+import time
+
+class Cliente():
+    def __init__(self,usu,contra):
+        self.HOST="127.0.0.1"
+        self.PORT=2121
+
+        ftp = FTP()
+        ftp.connect('127.0.0.1', 2121)
+        ftp.login(usu, contra, "noaccount")
+        try:
+            with open('README_COPIA.txt', 'wb') as f:
+                ftp.retrbinary('RETR README.txt', f.write)
+                f.write(str.encode(time.strftime("%H:%M:%S")))
+        except Exception as e:
+            print(e)
+        finally:
+            ftp.quit()
+
+print("Hola\nQuiere ingresar usuario y contrasenia?\n[S]i\t[N]o")
+op=input("Opcion: ")
+if op=="S":
+    name=input("Usuario: ")
+    contra=input("Contrasenia: ")
+    c=Cliente(name, contra)   
+else:
+    c=Cliente('anonymous','')

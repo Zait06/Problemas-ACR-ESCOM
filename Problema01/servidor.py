5089b91a108f08cb26be83971ebd888835365f29
Zait06 <zait-hdez@hotmail.com>
2019-10-22 13:47:56 -0500

    Servidor TCP

--- Problema01/servidor.py
+++ Problema01/servidor.py
@@ -6,46 +6,45 @@ import select
 import logging
 import threading
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='(%(threadName)-10s) %(message)s',
-)
+logging.basicConfig(level=logging.DEBUG,format='(%(threadName)-10s) %(message)s',)
 
 class Servidor():
-    def __init__(self):
-        self.HOST="127.0.0.1"; self.PORT=8080
-        self.buffer=1024; self.juga=0
+    def __init__(self,host,port,juga):
+        self.HOST=host; self.PORT=int(port)
+        self.juga=int(juga)
         self.serveraddr=(self.HOST,self.PORT)
-        self.listConec=list(); self.listHilos=[]	# Lista de conexiones recibidas e hilos
-        self.NUM_THREADS=5
-        self.barrera=threading.Barrier(self.NUM_THREADS)
+        self.listConec=list(); self.listHilos=list()	# Lista de conexiones recibidas e hilos
+        self.barrera=threading.Barrier(self.juga)    # Crear barrera
         
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.ServerTCP:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.ServerTCP:   # Crea socket TCP
             self.ServerTCP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
             self.ServerTCP.bind(self.serveraddr)
-            self.ServerTCP.listen(5)
+            self.ServerTCP.listen(self.juga)
             print("Servidor TCP a la escucha con direccion IP "+str(self.HOST))
             self.servirPorSiempre(self.ServerTCP)
 
     def servirPorSiempre(self,servidor):
-        logging.debug("Esperando instruccion...")
+        logging.debug("Esperando a los jugadores...")
         try:
-            i=0
-            while i<self.NUM_THREADS:
+            j=0
+            while j<self.juga:
                 conn,addr=servidor.accept()	# Coneccion y direccion del cliente
                 print("Conectado a: {}".format(addr))
                 self.listConec.append(conn)
-                hilo_jugador=threading.Thread(name='Jugador-'+str(i+1),
+                hilo_jugador=threading.Thread(name='Jugador-'+str(j+1),
                                                target=self.iniciarJuego,
-                                               args=(self.barrera,conn,addr),)
+                                               args=(conn,addr),)
                 self.listHilos.append(hilo_jugador)
-                i+=1
-            print(self.listHilos)
-            logging.debug("Jugadores conectados")
-            for t in range(self.NUM_THREADS):
-                self.listHilos[t].start()
-            while True:
-                self.gestion_conexiones()
+                j+=1
+
+            logging.debug("Creando juego")
+
+            for t in self.listHilos:
+                t.start()
+
+            #while True:
+                #self.gestion_conexiones()
+
         except Exception as e:
             print(e)
 
@@ -54,11 +53,11 @@ class Servidor():
             if conn.fileno()==-1:
                 self.listConec.remove(conn)
 
-    def iniciarJuego(self,barr,conn,addr):
+    def iniciarJuego(self,conn,addr):
         try:
             logging.debug("Recibiendo datos del cliente {} en el {}".format(addr))
             while True:
-                data=conn.recv(self.buffer)
+                data=conn.recv(1024)
                 
                 if data:
                     logging.debug("Analizando")
@@ -72,4 +71,4 @@ class Servidor():
         finally:
             conn.close()
 
-s=Servidor()
+s=Servidor(sys.argv[1],sys.argv[2],sys.argv[3])

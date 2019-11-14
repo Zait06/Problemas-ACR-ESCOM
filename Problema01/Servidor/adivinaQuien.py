a8bd322eb7d4d5b8d894f209ee626c668864ff27
Zait06 <zait-hdez@hotmail.com>
2019-11-14 11:52:15 -0600

    Adivina Quien

--- Problema01/Servidor/adivinaQuien.py
+++ Problema01/Servidor/adivinaQuien.py
@@ -13,3 +13,22 @@ class AdivinaQuien():
         self.personaje=""
         self.pistas=list()
         self.numJuga=numJuga
+        self.elegirPersonaje()
+
+    def elegirPersonaje(self):
+        a=rand.randrange(5)
+        if a==0:    self.personaje="Porfirio Diaz"
+        elif a==1:  self.personaje="Francisco I Madero"
+        elif a==2:  self.personaje=""
+        elif a==3:  self.personaje=""
+        elif a==4:  self.personaje=""
+
+    def pistaPersonaje(self,linea):
+        f=open(self.personaje,'rb')
+        i=1; psta=""
+        for linea in f.readlines():
+            if i==linea:
+                psta=linea
+                break
+            i+=1
+        return psta

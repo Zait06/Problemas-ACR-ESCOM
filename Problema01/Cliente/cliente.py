2b11c93d046910c45fa3eb317ab67c0470deb7db
Zait06 <zait-hdez@hotmail.com>
2019-11-14 11:07:49 -0600

    Cliente TCP

--- Problema01/Cliente/cliente.py
+++ Problema01/Cliente/cliente.py
@@ -6,12 +6,14 @@
 import os
 import sys
 import time
+import wave
 import socket
+import pyaudio
 
 class Cliente():
     def __init__(self,host,port):
         self.HOST=host; self.PORT=int(port)
-        self.dirArch=""
+        self.archivo="personaje.wav"
         
         with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as self.ClientTCP:
             self.ClientTCP.connect((self.HOST,self.PORT))
@@ -29,12 +31,54 @@ class Cliente():
             msgServer=self.ClientTCP.recvfrom(1024)    # Mensaje recibido del servidor
             msgRecib=msgServer[0].decode()   # Mensaje decodificado
             if msgRecib=='play':
-                self.dirArch=input("Ingrese direccion o nombre del achivo")
-                with open(self.dirArch, "rb") as archivo:
-                    content = archivo.read()
+                grab=input("Precione [G] para grabar el audio...")
+                if grab.upper()=='M':
+                    self.grabar()
+                else:
+                    print("Algo anda mal :c")
+                with open(self.archivo, "rb") as archivo:
+                    content = self.archivo.read()
                 self.ClientTCP.sendto(content,(self.HOST,self.PORT))  # Envia marca
             else:
                 print(msgRecib)
 
+    def grabar(self):
+        # Definicion de parametros
+        FORMAT=pyaudio.paInt16
+        CHANNELS=2
+        RATE=44100
+        CHUNK=1024
+        duracion=3
+
+        # Inicialización de Pyaudio
+        audio=pyaudio.PyAudio()
+        stream=audio.open(format=FORMAT,channels=CHANNELS,
+                            rate=RATE,input=True,
+                            frames_per_buffer=CHUNK)
+
+        print("Grabando...")
+        frames=[]
+
+        try:
+            # Inicio de grabacion
+            for i in range(0,int(RATE/CHUNK*duracion)):
+                data=stream.read(CHUNK)
+                frames.append(data)
+            print("Grabacion terminada")
+
+            # Fin de grabacion
+            stream.stop_stream()
+            stream.close()
+            audio.terminate()
+
+            # Creacion del archivo y guardado del mismo
+            waveFile=wave.open(self.archivo,'wb')
+            waveFile.setnchannels(CHANNELS)
+            waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+            waveFile.setframerate(RATE)
+            waveFile.writeframes(b''.join(frames))
+            waveFile.close()
+        except Exception as e:
+            print(e)
 
 c=Cliente(sys.argv[1],sys.argv[2])

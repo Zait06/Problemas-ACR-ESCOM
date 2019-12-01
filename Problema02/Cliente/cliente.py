99a098db9338c7b330a3aba8c2a57b1cdfe8c89d
Zait06 <zait-hdez@hotmail.com>
2019-11-30 20:17:15 -0600

    Cliente

--- Problema02/Cliente/cliente.py
+++ Problema02/Cliente/cliente.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import getpass
 import datetime
@@ -8,35 +9,63 @@ class Cliente():
         self.URL=url.lower().split("/")
         self.user=''; self.pasw=''
         self.orden=""; self.instruc=list()
+        self.servicio=""
         try:
+            os.system("cls")
             self.s=xmlrpc.client.ServerProxy('http://'+self.URL[2])  # Quien atiende las solicitudes
-            print("\tSERVICIO RPC")
-            s.eleccion(self.URL[3])
+            print("\t\tSERVICIO RPC")
+            self.servicio=self.s.eleccion(self.URL[3])
             nueva=input('Presione [M] si es que cuenta con un perfil, sino,\nprecione [N] para crear una nueva cuenta: ')
             self.user=input("Usuario: ")
             self.pasw=getpass.getpass("Contraseña: ")
 
             if nueva.upper()=='M':
-                ing=s.ingresar(user,pasw)
+                ing=self.s.ingresar(self.user,self.pasw,self.URL[3])
             else:
-                ing=s.registrar(user,pasw)
+                ing=self.s.registrar(self.user,self.pasw,self.URL[3])
 
-            if self.URL=="acceso-remoto":
+            print("\n\tServicio de "+self.servicio+". Sesión iniciada")
+            if self.URL[3]=="acceso-remoto":
                 self.accRem()
-            elif self.URL=="correo":
+            elif self.URL[3]=="correo":
                 self.correo()
-            elif self.URL=="archivos":
+            elif self.URL[3]=="archivos":
                 self.archivos()
         except Exception as e:
             print(e)
         
     def accRem(self):
-        print("Acceso Remoto")
+        print("jeje")
 
     def correo(self):
-        print("Correo")
+        nuser=self.user.split("@")
+        while True:
+            orden=input(nuser[0]+">> ")
+            instruc=orden.lower().split()
+            if instruc[0]=='sendzip':                                # Crear un archivo
+                print(self.s.crearArchivo(nuser[0],instruc[1],instruc[2]))
+            elif instruc[0]=='help' or instruc[0]=='?':             # Lista de comandos
+                print(self.s.ayudameCorr())
+            elif instruc[0]=='exit':                                # Salir de todo
+                print("\n\t\tHASTA PRONTO "+user+"\n")
+                break
+            else:
+                print("Instruccion incorrecta, intente de nuevo\n")
 
     def archivos(self):
-        print("Archivos")
+        while True:
+            orden=input("user@"+self.user+">> ")
+            instruc=orden.lower().split()
+            if instruc[0]=='create':                                # Crear un archivo
+                print(self.s.crearArchivo(instruc[1],self.user))
+            elif instruc[0]=='readdir':                             # Vista de todos los archivos
+                print(self.s.verContenido(self.user))
+            elif instruc[0]=='help' or instruc[0]=='?':             # Lista de comandos
+                print(self.s.ayudameArch())
+            elif instruc[0]=='exit':                                # Salir de todo
+                print("\n\t\tHASTA PRONTO "+user+"\n")
+                break
+            else:
+                print("Instruccion incorrecta, intente de nuevo\n")
 
 c=Cliente(sys.argv[1])
